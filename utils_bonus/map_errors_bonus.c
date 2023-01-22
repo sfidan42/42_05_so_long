ec9a83f7adc582be879f14c803558bbd4dff2b90
Sadettin-38 <sadettinfidan00@gmail.com>
2023-01-22 22:40:20 +0300

    project is finished, leaks and bugs are to be checked

--- utils_bonus/map_errors_bonus.c
+++ utils_bonus/map_errors_bonus.c
@@ -1,37 +1,70 @@
-#include "../so_long_bonus.h"
+#include "../so_long.h"
+
+int	ft_items(t_map m, t_player *h)
+{
+	int	i;
+	int	j;
+	int	c;
+
+	i = 0;
+	c = 0;
+	h->loc_x = 0;
+	h->loc_y = 0;
+	while (m.map[i])
+	{
+		j = 0;
+		while (m.map[i][j])
+		{
+			c += m.map[i][j] == 'c';
+			if (m.map[i][j] == 'p')
+			{
+				h->loc_x = i;
+				h->loc_y = j;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (c);
+}
 
 int	ft_collectibles(t_map m, int i, int j)
 {
 	int	item;
 
-	if (i < 0 || j < 0 || i > m.size_x || j > m.size_y)
-		return ;
-	if (m.map[i][j] == '1')
-		return ;
-	if (m.map[i][j] == '2')
-		return ;
 	item = m.map[i][j] == 'c';
+	if (i < 0 || j < 0 || i > m.size_x || j > m.size_y)
+		return (0);
+	if (m.map[i][j] == '1')
+		return (0);
+	if (m.map[i][j] == '2')
+		return (0);
 	m.map[i][j] = '2';
-	if (item)
-		return (1
-			+ ft_collectibles(m, i + 1, j)
-			+ ft_collectibles(m, i - 1, j)
-			+ ft_collectibles(m, i, j + 1)
-			+ ft_collectibles(m, i, j - 1));
-	return (0);
+	return (item
+		+ ft_collectibles(m, i + 1, j)
+		+ ft_collectibles(m, i - 1, j)
+		+ ft_collectibles(m, i, j + 1)
+		+ ft_collectibles(m, i, j - 1));
 }
 
 char	**ft_check_map(t_pdmh pdmh)
 {
-	if (!m.size_x)
+	t_map		m;
+	t_player	h;
+	int			items;
+
+	m = *pdmh.m;
+	h = *pdmh.h;
+	items = ft_items(m, &h);
+	if (!pdmh.m->size_x)
 		ft_printf("Error! The map is empty!\n");
-	if (ft_collectibles(pdmh.m, pdmh.h.loc_x, pdmh.h.loc_y) - pdmh.m.items)
-		ft_printf("Error! There are item(s) cannot be collected!\n");
-	if (ft_map_misshapen(m.map))
+	else if (ft_collectibles(m, h.loc_x, h.loc_y) - items && h.loc_x && h.loc_y)
+		ft_printf("Error! There are %d items cannot be collected!\n", items);
+	else if (ft_map_misshapen(m.map))
 		ft_printf("Error! The map is not rectangular!\n");
-	if (ft_map_uninclosed(m.map))
+	else if (ft_map_uninclosed(m.map))
 		ft_printf("Error! The map is not surrounded by walls!\n");
-	if (ft_map_objects(m.map))
+	else if (ft_map_objects(m.map))
 		return (m.map);
 	ft_free_map(m.map);
 	exit(0);
